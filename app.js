950d017ce910736bbfd4c03c8db5696e59cfedd2
Chad Hinds <chadrickj8@gmail.com>
2019-08-22 22:04:48 -0400

    Initial setup of v1/tours route

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
+const fs = require('fs')
 const express = require('express');
 
 const app = express();
+// read tour data
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
 // Defining routes
-app.get('/', (req, res) => {
-    res.status(200).json({ message: "Booty from the server side!",
-    app: 'Natours'});   
-});
-
-app.post('/', (req, res) =>{
-    res.send("You can post to this endpoint...")
+app.get("/api/v1/tours", (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tours
+        }
+    });
 })
+
 const port = 3000;
 app.listen(port, () =>{
     console.log(`App running on port ${port}`);
