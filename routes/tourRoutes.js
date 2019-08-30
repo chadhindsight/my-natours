67af11d22b396a3c5753cdc1a9f7b79af727923c
Chad Hinds <chadrickj8@gmail.com>
2019-08-30 13:03:32 -0400

    place module export on last line

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,10 +3,7 @@ const tourController = require('./../controllers/tourController');
 
 const router = express.Router();
 
-router.param('id', (req, res, next) =>{
-    console.log(`Tour id is: ${id}`);
-    next();
-})
+router.param('id', tourController.checkID);
 
 router.route(`/`).get(tourController.getAllTours).post(tourController.createTour);
 router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
