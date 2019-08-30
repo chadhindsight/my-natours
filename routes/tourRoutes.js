f01256fdbcc2b4d34d40e6c76df358ce5d0837e7
Chad Hinds <chadrickj8@gmail.com>
2019-08-30 13:45:12 -0400

    initially setup checkBody middleware

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,6 +5,11 @@ const router = express.Router();
 
 router.param('id', tourController.checkID);
 
+// CheckBody middleware checks for name and price properties in the body
+const checkBody = (req, res, next, va) => {
+
+}
+
 router.route(`/`).get(tourController.getAllTours).post(tourController.createTour);
 router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
 
