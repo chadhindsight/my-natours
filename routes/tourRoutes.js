93c0b7ea2b129de5d969b96848b1936cf5dad3d5
Chad Hinds <chadrickj8@gmail.com>
2019-08-30 13:55:32 -0400

    move checkBody to tourControllers file

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,12 +5,8 @@ const router = express.Router();
 
 router.param('id', tourController.checkID);
 
-// CheckBody middleware checks for name and price properties in the body
-const checkBody = (req, res, next, va) => {
 
-}
-
-router.route(`/`).get(tourController.getAllTours).post(tourController.createTour);
+router.route(`/`).get(tourController.getAllTours).post(checkBody, tourController.createTour);
 router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
 
 module.exports = router;
