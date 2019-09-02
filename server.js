bc78b2ecae9d4cfe102683cfa1fd53b6a8e1198e
Chad Hinds <chadrickj8@gmail.com>
2019-09-02 15:56:50 -0400

    require dotenv before app file so it is read first

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 // This file is only invovles server stuff like ports, etc. Express stuff is done in app.js
-const app = require('./app');
 const dotenv = require('dotenv');
-
 dotenv.config({path: './config.env'});
 
-console.log(process.env)
-const port = 3000;
+const app = require('./app');
+
+console.log(process.env);
+
+const port =  process.env.PORT || 8000;
 app.listen(port, () => {
     console.log(`App running on port ${port}`);
 });
