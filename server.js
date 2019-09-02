d6ecad2f931974024ce79bc1dff521b7517f2f4d
Chad Hinds <chadrickj8@gmail.com>
2019-09-02 15:34:31 -0400

    add dotenv to server file

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // This file is only invovles server stuff like ports, etc. Express stuff is done in app.js
 const app = require('./app');
+const dotenv = require('dotenv');
 
 const port = 3000;
 app.listen(port, () => {
