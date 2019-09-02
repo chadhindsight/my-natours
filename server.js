f63a5ae110f529de764c916bd80252c6c411779a
Chad Hinds <chadrickj8@gmail.com>
2019-09-02 15:37:36 -0400

    log info about your env variable to make sure it is setup

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 const app = require('./app');
 const dotenv = require('dotenv');
 
+dotenv.config({path: './config.env'});
+
+console.log(process.env)
 const port = 3000;
 app.listen(port, () => {
     console.log(`App running on port ${port}`);
