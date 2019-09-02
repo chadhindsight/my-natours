f20486f00815feb1ba2142853957a08b5c401535
Chad Hinds <chadrickj8@gmail.com>
2019-09-02 15:40:48 -0400

    fix typo in express.static call

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 // MIDDLEWARES SECTION!
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.static(`${_dirname}/public`));
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) =>{
     console.log('Hello from the middleware');
