308886a1e56b437e5f0c4abb517d45f8f3fec559
Chad Hinds <chadrickj8@gmail.com>
2019-09-05 12:59:44 -0400

    change console log message for port 3000

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = require('./app');
 
 console.log(process.env);
 
-const port =  process.env.PORT || 8000;
+const port =  process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`App running on port ${port}`);
+    console.log(`App  is running on port ${port}!`);
 });
