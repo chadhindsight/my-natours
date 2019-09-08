c06f7d80b343f8c5be1660220dd9f7ac8ea58a16
Chad Hinds <chadrickj8@gmail.com>
2019-09-08 10:06:11 -0400

    Remove console log message with process.env as it was not needed

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ const app = require('./app');
 dotenv.config({path: './config.env'});
 
 
-console.log(process.env);
-
 const port =  process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`App  is running on port ${port}!`);
