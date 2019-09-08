6ec3bb1432ddebb3610b597f00d964de23cb5916
Chad Hinds <chadrickj8@gmail.com>
2019-09-08 10:03:52 -0400

    place app call on an earlier line so things seem more streamlined to read

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 // This file is only invovles server stuff like ports, etc. Express stuff is done in app.js
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
+
 // Allows you to specify the file location to configure you environment varialbe
 dotenv.config({path: './config.env'});
 
-const app = require('./app');
 
 console.log(process.env);
 
