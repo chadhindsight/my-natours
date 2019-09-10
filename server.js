5023238b4e9ea341ccc68a752bdaead24371f662
Chad Hinds <chadrickj8@gmail.com>
2019-09-10 17:38:20 -0400

    comment out test database logic

--- server.js
+++ server.js
@@ -17,29 +17,19 @@ mongoose
     })
     .then(() => console.log('DB connection successful!'));
 
-const tourSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'A tour must have a name'],
-        unique: true
-    },
-    rating: {
-        type: Number,
-        default: 4.5
-    },
-    price: {
-        type: Number,
-        required: [true, 'A tour must have a price']
-    }
-})
 
-const Tour = mongoose.model('Tour', tourSchema);
 
-const testTour = new Tour({
-    name: 'The Forest Hiker',
-    rating: 4.7,
-    price: 469
-})
+// const testTour = new Tour({
+//     name: 'The Park Camper',
+//     rating: 4.7,
+//     price: 500
+// })
+
+// testTour.save().then(doc =>{
+//     console.log(doc);
+// }).catch(err =>{
+//     console.log('ERROR', err)
+// });
 
 const port =  process.env.PORT || 3000;
 app.listen(port, () => {
