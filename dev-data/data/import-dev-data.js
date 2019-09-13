d7488cfe0a7fb1d03e8e1a61fd11adde1e84e24d
Chad Hinds <chadrickj8@gmail.com>
2019-09-13 15:14:59 -0400

    setup file to start using dev data

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('./../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+    '<password>',
+    process.env.DATABASE_PW);
+
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    })
+    .then(() => console.log('DB connection successful!'));
+
+    // READ JSON FILE
+    const tours = JSON.parse(fs.readFileSync('tours-simple.json', 'utf-8'));
+
+    // IMPORT DATA FROM DB
+    const importData = async () =>{
+        try {
+            await Tour.create(tours);
+
+        }
+        catch(err) {
+            console.log(err);
+        }
+    }
