2dbfaff87bbf1e13d18098eb0bcad85d0f3ef702
Chad Hinds <chadrickj8@gmail.com>
2019-09-13 09:14:10 -0400

    add more properties to data model

---
+++ models/tourModels.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'],
+        unique: true,
+        trim: true
+    },
+    duration: {
+        type: Number,
+        required: true,
+        message: "Tour must have a duration"
+    },
+    maxGroupSize: {
+        type: Number,
+        required: true,
+        message: "Tour must have a group size"
+    },
+    difficulty:{
+        type: String,
+        required: [true, 'A tour must have a difficulty']
+    },
+    ratingsAverage: {
+        type: Number,
+        default: 4.5
+    },
+    ratingsQuantity: {
+        type: Number,
+        default: 0
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price']
+    },
+    priceDiscount: Number,
+    summary: {
+        type: String,
+        trim: true
+    }
+})
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+module.exports = Tour;
