606912ead04d5340d387a534dd812aaf31317a00
Chad Hinds <chadrickj8@gmail.com>
2019-09-19 10:57:27 -0400

    set select to false which will hide createdAt field

--- models/tourModels.js
+++ models/tourModels.js
@@ -56,9 +56,11 @@ const tourSchema = new mongoose.Schema({
     images: [String],
     createAt: {
         type: Date,
-        default: Date.now()
-    }
-})
+        default: Date.now(),
+        select: false
+    },
+    startDates: [Date]
+});
 
 const Tour = mongoose.model('Tour', tourSchema);
 
