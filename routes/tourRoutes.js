d1cd31e815133ffd25716f06f77290a5ffcdf3bd
Chad Hinds <chadrickj8@gmail.com>
2019-09-23 14:59:29 -0400

    create a route that leads to top 5 cheapest tours

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,7 +4,7 @@ const tourController = require('./../controllers/tourController');
 const router = express.Router();
 
 // All the related routes & controllers being used with them!
-router.route('top-5-cheap');
+router.route('top-5-cheap').get(tourController.getAllTours);
 router.route(`/`).get(tourController.getAllTours).post(tourController.createTour);
 router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
 
