72894c517ba55f2b6d254a13493576ccb93d764d
Chad Hinds <chadrickj8@gmail.com>
2019-09-24 15:05:21 -0400

    add missing / to top  route

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,8 +3,8 @@ const tourController = require('./../controllers/tourController');
 
 const router = express.Router();
 
-// All the related routes & controllers being used with them!
-router.route('top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours);
+// All the related routes & controllers being used with them
+router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours);
 router.route(`/`).get(tourController.getAllTours).post(tourController.createTour);
 router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
 
