271909427a2014f77aa1c3f2badee38e6e534c59
Chad Hinds <chadrickj8@gmail.com>
2019-09-28 14:56:54 -0400

    place route for tour stats before top 5 route

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -4,8 +4,10 @@ const tourController = require('./../controllers/tourController');
 const router = express.Router();
 
 // All the related routes & controllers being used with them
-router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours);
 router.route('/tour-stats').get(tourController.getTourStats);
+router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+
 router.route(`/`).get(tourController.getAllTours).post(tourController.createTour);
 router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
 
