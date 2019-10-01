723b607dd13e740bfbfac6b3271dd38fd90820f5
Chad Hinds <chadrickj8@gmail.com>
2019-10-01 12:45:00 -0400

    Query Middleware for secret tours

--- models/tourModels.js
+++ models/tourModels.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const slugify = require('slugify');
+
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -8,6 +10,7 @@ const tourSchema = new mongoose.Schema({
         unique: true,
         trim: true
     },
+    slug: String,
     duration: {
         type: Number,
         required: true,
@@ -59,7 +62,11 @@ const tourSchema = new mongoose.Schema({
         default: Date.now(),
         select: false
     },
-    startDates: [Date]
+    startDates: [Date],
+    secretTour: {
+        type: Boolean,
+        default: false
+    }
 }, {
    toJson: {virtuals: true},
    toObject: {virtuals: true} 
@@ -68,10 +75,17 @@ const tourSchema = new mongoose.Schema({
 tourSchema.virtual('durationWeeks').get(function(){
     return this.duration / 7;
 })
+// This runs before .save() and .create() are run
+tourSchema.pre('save', function(next){
+    this.slug = slugify(this.name, {lower: true });
 
-tourSchema.pre('save', function(){
-
+    next();
 })
+// Query Middleware for secret tours
+tourSchema.pre('find', function () {
+    this.find({secretTour: {$ne: true} });
+    next();
+});
 
 const Tour = mongoose.model('Tour', tourSchema);
 
