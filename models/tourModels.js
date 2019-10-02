fedc11568640d7810b9edca27c6adf19dfce1e4d
Chad Hinds <chadrickj8@gmail.com>
2019-10-02 10:36:46 -0400

    add a new stage to the pipeline and put it in aggregation middleware

--- models/tourModels.js
+++ models/tourModels.js
@@ -95,8 +95,9 @@ tourSchema.post(/^find/, function (docs, next) {
 });
 // Aggregation Middleware!
 tourSchema.pre('aggregate', function (next) {
-    console.log(this);
-    next()
+    this.pipeline().unshift({$match: { secretTour: {$ne: true} }});
+    console.log(this.pipeline());
+    next();
 })
 
 const Tour = mongoose.model('Tour', tourSchema);
