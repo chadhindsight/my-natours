180457bb85750c87278cf6bfc1fe582bbc2dc7ba
Chad Hinds <chadrickj8@gmail.com>
2019-10-11 08:39:01 -0400

    comment on use of validator middleware

--- models/tourModels.js
+++ models/tourModels.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const slugify = require('slugify');
+// check conditions for schema properties
 const validator = require('validator');
 
 
