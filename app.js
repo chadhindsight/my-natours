cc695a131345f42207d390fc03fcdc1fcac0bc7d
Chad Hinds <chadrickj8@gmail.com>
2019-10-11 19:42:58 -0400

    setup error message for unhandled routes

--- app.js
+++ app.js
@@ -29,5 +29,12 @@ app.use((req, res, next) =>{
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+// Runs for all uncatched http verbs
+app.all('*', (req, res, next) =>{
+    res.status(404).json({
+        status: 'fail',
+        message: `Can't find ${req.originalUrl} on server`
+    })
+})
 
 module.exports = app;
