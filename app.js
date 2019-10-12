83d1cb7f94bde34d6d60d3cda59b68a4fb784d90
Chad Hinds <chadrickj8@gmail.com>
2019-10-12 13:03:50 -0400

    comment on how app.all is being used

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
+
 const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -30,27 +32,10 @@ app.use((req, res, next) =>{
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-// Runs for all uncatched http verbs
+// This runs for all uncatched http routes and related verbs
 app.all('*', (req, res, next) =>{
-    // res.status(404).json({
-    //     status: 'fail',
-    //     message: `Can't find ${req.originalUrl} on server`
-    // })
-    const err = new Error(`Can't find ${req.originalUrl} on server`)
-    err.status = 'fail'
-    err.statusCode = 404;
-
-    next(err);
+    next(new AppError(`Can't find ${req.originalUrl} on server`));
 });
 
-app.use((err, req, res, next) =>{
-    //Set the error equal to whatever error is received!
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error'
-
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    });
-})
+app.use(globalErrorHandler);
 module.exports = app;
