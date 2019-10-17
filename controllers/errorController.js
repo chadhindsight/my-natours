4f4149aad514bb100979c606e627055bab5df230
Chad Hinds <chadrickj8@gmail.com>
2019-10-17 16:33:18 -0400

    Create handleError function for database related operational errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,9 @@
+const AppError = require('./../utils/appError');
+const handleCastErrorDB = err => {
+    const message = `invalid ${err.path}: ${err.value}`
+    return new AppError(message, 400);
+}
+
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
