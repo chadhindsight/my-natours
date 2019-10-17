0787cb070d61cedc969dcf17eab472749fdb15e9
Chad Hinds <chadrickj8@gmail.com>
2019-10-17 11:51:17 -0400

    fix spacing in send error dev

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,26 +1,29 @@
-const sendErrorDev = (err, res) =>{
-//    OPerational Error message for client
-    if(err.isOperational) {
-       res.status(err.statusCode).json({
-           status: err.status,
-           message: err.message,
-           stack: error.stack
-       }) 
-   }
-//    Error message not meant for the user
-   else {
-       res.status(500).json({
-           status: 'error',
-           message: 'Something went wrong!'
-       })
-   }
+const sendErrorDev = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack
+    });
 }
 
 const sendErrorProd = ( err, res) =>{
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    })
+    // OPerational Error message for client
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message
+        })
+    }
+    // Error message not meant for the user
+    else {
+        console.log('Error', err);
+
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went wrong!'
+        })
+    }
 }
 
 module.exports = (err, req, res, next) => {
@@ -32,7 +35,11 @@ module.exports = (err, req, res, next) => {
       sendErrorDev(err)
     } 
     else if (process.env.NODE_ENV === 'production') {
-     sendErrorProd(err)   
+        let error  = {...err};
+
+        if(error.name === 'CastError') {error = handleCastErrorDB(error)}
+     
+        sendErrorProd(err, res)   
     }
    
-}
+};
