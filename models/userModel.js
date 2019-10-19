c3ccc0e2a50d821531ffde93026214fb5e9cd46e
Chad Hinds <chadrickj8@gmail.com>
2019-10-19 15:22:30 -0400

    set required fields for user schema

--- models/userModel.js
+++ models/userModel.js
@@ -1,1 +1,23 @@
 const mongoose = require('mongoose');
+// name, email, photo, pw, pwconfirm
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please tell us your name"]
+    },
+    email: {
+        type: String,
+        required: [true, "Please tell us your name"],
+        unique: true,
+        // transform all emails to lower
+        lowercase: true
+    },
+    photo: String,
+    pw: String,
+    pwconfirm: Boolean
+})
+
+const User = mongoose.model("User", userSchema);
+//Export user model so it can be used in other files
+module.exports = User;
