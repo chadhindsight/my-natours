1405615c7883ac5ee803c0551d7ec7c5f11272fc
Chad Hinds <chadrickj8@gmail.com>
2019-10-19 13:54:50 -0400

    put code for uncaught rejection handling further up

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-// This file only invovles server stuff like ports, etc. Express stuff is done in app.js
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-dotenv.config({path: './config.env'});
+process.on('uncaughtException', err => {
+    console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+    console.log(err.name, err.message);
+    process.exit(1);
+});
+
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
-    '<password>', 
-    process.env.DATABASE_PW);
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+);
 
 mongoose
     .connect(DB, {
@@ -17,23 +23,15 @@ mongoose
     })
     .then(() => console.log('DB connection successful!'));
 
-const port =  process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`App  is running on port ${port}!`);
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+    console.log(`App running on port ${port}...`);
 });
 
-process.on('unhandledRejection', err =>{
-    console.log('UNHANDLED REJECTION! Shutting down...')
-    console.log(err);
-    ServiceWorkerRegistration.close(()=>{
-        process.exit(1);
-    })
-})
-
-process.on('uncaughtException', err =>{
-    console.log('UNHANDLED Exception! Shutting down...')
+process.on('unhandledRejection', err => {
+    console.log('UNHANDLED REJECTION! 💥 Shutting down...');
     console.log(err.name, err.message);
-    ServiceWorkerRegistration.close(() => {
+    server.close(() => {
         process.exit(1);
-    })
-})
+    });
+});
