2a4a2fca74fd7cc15db2355671f18a3e0fe2c9d4
Chad Hinds <chadrickj8@gmail.com>
2019-10-20 10:12:03 -0400

    describe how the signuup async function works in comments

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -1,5 +1,6 @@
 const User = require('./../models/userModel');
 
 exports.signup = async (req, res, next) =>{
-
+    //Create a new user doc based on the user model that will have the fields populated by the values from req.body
+    const newUser = await User.create(req.body)
 }
