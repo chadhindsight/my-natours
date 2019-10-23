49e085b92fdfa2aa81b5b848ec22bbc8ffc5771d
Chad Hinds <chadrickj8@gmail.com>
2019-10-23 14:17:01 -0400

    replace newUser logic with more secure layout

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -1,6 +1,18 @@
 const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
 
-exports.signup = async (req, res, next) =>{
+exports.signup = catchAsync(async(req, res, next) =>{
     //Create a new user doc based on the user model that will have the fields populated by the values from req.body
-    const newUser = await User.create(req.body)
-}
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        pw: req.body.pw,
+        pwconfirm: req.body.pwconfirm
+    });
+    res.status(200).json({
+        status: 'success',
+        data: {
+            user: newUser
+        }
+    });
+});
