749cc1103fa5f8dc15626e841904dab28d8c1ce7
Chad Hinds <chadrickj8@gmail.com>
2019-10-24 14:00:39 -0400

    add login route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,13 @@
 express = require('express');
 const userController = require('./../controllers/userController');
+const authenticationController = require('./../controllers/authenticationController');
+
 router = express.Router();
 
+router.post('/signup', authenticationController.signup);
+router.post('/login', authenticationController.login);
 
 router.route('/').get(userController.getAllUsers).post(userController.createUser);
 router.route('/:id').get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser);
-
+// export the router so it can be used in app.js
 module.exports = router;
