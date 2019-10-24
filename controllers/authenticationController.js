9bcb0604815e7d0821c8b6d35914c1f6fc706867
Chad Hinds <chadrickj8@gmail.com>
2019-10-24 14:15:54 -0400

    Set model to not automatically display password when user signs in

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken')
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
 exports.signup = catchAsync(async(req, res, next) =>{
     //Create a new user doc based on the user model that will have the fields populated by the values from req.body
@@ -24,7 +25,19 @@ exports.signup = catchAsync(async(req, res, next) =>{
     });
 });
 
-exports.login = (req,res,next) =>{
-    const { email, password } = req.body;
+exports.login = (req,res,next) => {
+    const { email, pw } = req.body;
 
+    // Check if email and password exist
+    if(!email || !pw) {
+        return next(new AppError("Please provide email & password", 400));
+    }
+    // Check if user exists & password is right
+    const user = UserfindOne({email});
+
+    const token = '';
+    res.status(200).json({
+        status: 'success',
+        token
+    });
 }
