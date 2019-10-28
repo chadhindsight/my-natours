531d45026f9f412e8506a2c8c047e4e2d7813407
Chad Hinds <chadrickj8@gmail.com>
2019-10-28 09:15:32 -0400

    add auth middleware to tour routes

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
+const authenticationController = require('./../controllers/authenticationController');
+
 
 const router = express.Router();
 
@@ -8,7 +10,7 @@ router.route('/tour-stats').get(tourController.getTourStats);
 router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours);
 router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
-router.route(`/`).get(tourController.getAllTours).post(tourController.createTour);
+router.route(`/`).get(authenticationController,tourController.getAllTours).post(tourController.createTour);
 router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
 
 module.exports = router;
