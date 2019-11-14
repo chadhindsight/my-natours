c12b87ad0135fc85250bdbc21a8abe085e11cf50
Chad Hinds <chadrickj8@gmail.com>
2019-11-13 23:59:02 -0500

    re-structure verification token

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -50,31 +50,44 @@ exports.login = catchAsync(async(req,res,next) => {
     });
 });
 
-exports.protect = catchAsync(async(req, res, next) =>{
-    // Get the token and check if it exists
-    let token; 
-    
-    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-         token = req.headers.authorization.split(' ')[1];
-    } 
-
-    if(!token) {
-        return next( new AppError('You are not logged in. Please log in to get access', 401));
+exports.protect = catchAsync(async (req, res, next) => {
+    // 1) Getting token and check of it's there
+    let token;
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith('Bearer')
+    ) {
+        token = req.headers.authorization.split(' ')[1];
     }
-    // Validate the token with the verify method from jwt
-    const decoded = promisify(jwt.verify)(token, process.env.JWT_SECTRET);
-    console.log(decoded)
 
-    //Check if the user still exists. This is trash
-    const freshUser = await User.findById(decoded.id);
-    if(!freshUser) {
-        return next(new AppError('User no longer exists!', 401))
+    if (!token) {
+        return next(
+            new AppError('You are not logged in! Please log in to get access.', 401)
+        );
     }
-    // Check if user has changed password after getting the token
-    if(freshUser.changedPWAfter(decoded.iat)){
+
+    // 2) Verification token!
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+    // 3) Check if user still exists
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
+        return next(
+            new AppError(
+                'The user belonging to this token does no longer exist.',
+                401
+            )
+        );
+    }
+
+    // 4) Check if user changed password after the token was issued
+    if (currentUser.changedPWAfter(decoded.iat)) {
         return next(
             new AppError('User recently changed password! Please log in again.', 401)
         );
-    };
+    }
+
+    // GRANT ACCESS TO PROTECTED ROUTE
+    req.user = currentUser;
     next();
-})
+});
